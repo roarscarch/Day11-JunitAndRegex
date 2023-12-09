1e2047e2ebede3011232b1076905383b68c5ba37
roarscarch <2020ugee082@nitjsr.ac.in>
2023-12-09 13:18:35 +0530

    Added UC1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Regex and Junit Class");
+        Scanner scanner = new Scanner(System.in);
+
+        // ^? what is this and what is $?
+        String firstNameRegex = "^[A-Z]{1,1}[a-z]{2,}$";
+
+        System.out.print("Enter your first name: ");
+        String firstName = scanner.nextLine();
+
+        while (!firstName.matches(firstNameRegex)) {
+            System.out.println("Invalid first name. Please enter a valid first name.");
+            System.out.print("Enter your first name: ");
+            firstName = scanner.nextLine();
+        }
+
+        System.out.println("Valid first name: " + firstName);
+
+        scanner.close();
     }
 }
