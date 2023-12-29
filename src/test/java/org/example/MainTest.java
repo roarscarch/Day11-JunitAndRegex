1b15c95f6e03db89853468d53e4fd827ab95bc1f
roarscarch <2020ugee082@nitjsr.ac.in>
2023-12-29 21:12:31 +0530

    test recreated

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -3,39 +3,21 @@ package org.example;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.example.Exceptions.InvalidEmailException;
+import org.example.Exceptions.InvalidPhoneNumberException;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+
+import org.example.Validator;
+import org.example.Exceptions.InvalidEmailException;
+import org.example.Exceptions.InvalidFirstNameException;
+import org.example.Exceptions.InvalidPasswordException;
+import org.example.Exceptions.InvalidPhoneNumberException;
 public class MainTest {
 
     // Custom exception for invalid email
-    public static class InvalidEmailException extends RuntimeException {
-        public InvalidEmailException(String message) {
-            super(message);
-        }
-    }
-
-    // Custom exception for invalid first name
-    public static class InvalidFirstNameException extends RuntimeException {
-        public InvalidFirstNameException(String message) {
-            super(message);
-        }
-    }
-
-    // Custom exception for invalid password
-    public static class InvalidPasswordException extends RuntimeException {
-        public InvalidPasswordException(String message) {
-            super(message);
-        }
-    }
-
-    // Custom exception for invalid phone number
-    public static class InvalidPhoneNumberException extends RuntimeException {
-        public InvalidPhoneNumberException(String message) {
-            super(message);
-        }
-    }
-
     @ParameterizedTest
     @ValueSource(strings = {
             "abc@yahoo.com",
@@ -63,83 +45,51 @@ public class MainTest {
     })
     public void testEmails(String email) {
         try {
-            validateEmail(email);
-        } catch (InvalidEmailException exception) {
+            Validator.validateEmail(email);
+        } catch (org.example.Exceptions.InvalidEmailException exception) {
             fail("Expected InvalidEmailException");
         }
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "Anurag", "Kumar", "Dubey" })
+    @ValueSource(strings = { "Anur", "Dubey", "Ragnu", "Anurag", "Mnurag" })
     public void testFirstName(String name) {
-        try {
-            validateFirstName(name);
-        } catch (InvalidFirstNameException exception) {
-            fail("Expected InvalidFirstNameException");
-        }
+        String nameRegex = "^[A-Z][a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(nameRegex);
+        Matcher matcher = pattern.matcher(name);
+        assertTrue(matcher.matches());
     }
 
     @ParameterizedTest
     @ValueSource(strings = {
-
             "123@Pass",
             "Or456@345"
     })
     public void testPassword(String password) {
-        try {
-            validatePassword(password);
-        } catch (InvalidPasswordException exception) {
-            fail("Expected InvalidPasswordException");
-        }
+
+        String passwordRegex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z\\d]).{8,}$";
+        Pattern pattern = Pattern.compile(passwordRegex);
+        Matcher matcher = pattern.matcher(password);
+        assertTrue(matcher.matches());
     }
 
     @ParameterizedTest
     @ValueSource(strings = {
+            "123456",
+            "+12 345678898",
 
-            "12 345678898",
-            "91 9262509652"
+            "abcdedfcg",
+            "+91 78767676777777",
+            "+98 767367363err"
     })
-    public void checkPhone(String phone) {
+    public void checkPhoneSad(String phone) {
         try {
-            validatePhoneNumber(phone);
-        } catch (InvalidPhoneNumberException exception) {
+            Validator.validatePhoneNumber(phone);
             fail("Expected InvalidPhoneNumberException");
+        } catch (org.example.Exceptions.InvalidPhoneNumberException exception) {
+            assertEquals("Phone number is invalid", exception.getMessage());
         }
     }
 
-    private void validateEmail(String email) {
-        String emailRegex = "^[a-zA-Z0-9.$&%_+-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z]{2,})+$";
-        Pattern pattern = Pattern.compile(emailRegex);
-        Matcher matcher = pattern.matcher(email);
-        if (!matcher.matches()) {
-            throw new InvalidEmailException("Email is invalid");
-        }
-    }
 
-    private void validateFirstName(String name) {
-        String nameRegex = "^[A-Z][a-zA-Z]{2,}$";
-        Pattern pattern = Pattern.compile(nameRegex);
-        Matcher matcher = pattern.matcher(name);
-        if (!matcher.matches()) {
-            throw new InvalidFirstNameException("First name is invalid");
-        }
-    }
-
-    private void validatePassword(String password) {
-        String passwordRegex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z\\d]).{8,}$";
-        Pattern pattern = Pattern.compile(passwordRegex);
-        Matcher matcher = pattern.matcher(password);
-        if (!matcher.matches()) {
-            throw new InvalidPasswordException("Password is invalid");
-        }
-    }
-
-    private void validatePhoneNumber(String phone) {
-        String phoneRegex = "^[0-9]{1,4} [0-9]{5,15}$";
-        Pattern pattern = Pattern.compile(phoneRegex);
-        Matcher matcher = pattern.matcher(phone);
-        if (!matcher.matches()) {
-            throw new InvalidPhoneNumberException("Phone number is invalid");
-        }
-    }
 }
